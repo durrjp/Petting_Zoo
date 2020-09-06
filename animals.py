0e3bf0240970e1a437f5368cfd97c213ca13178b
durrjp <durrjp1@gmail.com>
2020-09-06 11:11:02 -0500

    dynamic property

--- animals.py
+++ animals.py
@@ -2,15 +2,28 @@ from datetime import date
 
 class Llama:
 
-    def __init__(self, name, species, shift, food):
+    def __init__(self, name, species, shift, food, chip_number):
         # Establish the properties of each animal
         # with a default value
         self.name = name
         self.species = species
         self.shift = shift
         self.food = food
+        self.__chip_number = chip_number
         self.date_added = date.today()
         self.walking = True
+    # propert = getter
+    @property
+    def name_spec(self):
+        return f"{self.name} {self.species}"
+
+    @property
+    def chip_number(self):
+        return self.__chip_number
+
+    @chip_number.setter
+    def chip_number(self, number):
+        pass
 
     def feed(self):
         print(f'{self.name} was fed {self.food} on {date.today().strftime("%m/%d/%Y")}')
@@ -36,6 +49,7 @@ class Pig:
     def __str__(self):
         return f"{self.name} is a {self.species}"
 
-miss_fuzz = Llama("Miss Fuzz", "tall llama", "afternoon", "llama feed")
+miss_fuzz = Llama("Miss Fuzz", "tall llama", "afternoon", "llama feed", 102)
 print(miss_fuzz.feed())
 print(miss_fuzz)
+print(miss_fuzz.name_spec)
